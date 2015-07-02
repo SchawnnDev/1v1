749c400558aae0c12aa73cd1bfc5d935b4dc9e63
SchawnnDev <iperskill@gmail.com>
2015-07-02 15:57:48 +0200

    Created repo, first commit

---
+++ src/main/java/fr/schawnndev/Main.java
@@ -0,0 +1,33 @@
+/*
+ * ******************************************************
+ *  * Copyright (C) 2015 SchawnnDev <contact@schawnndev.fr>
+ *  *
+ *  * This file (fr.schawnndev.Main) is part of 1v1.
+ *  *
+ *  * Created by SchawnnDev on 02/07/15 15:53.
+ *  *
+ *  * 1v1 can not be copied and/or distributed without the express
+ *  * permission of SchawnnDev.
+ *  ******************************************************
+ */
+
+package fr.schawnndev;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *  J'aurais voulu la nommer 1v1 mais en java on ne peut pas créer de classe avec des nombres ..
+ */
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
